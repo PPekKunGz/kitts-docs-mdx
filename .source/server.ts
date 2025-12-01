e0d2972ade19bba1fe1b1ae3936e80b2761a3060
PPekKunGzDev <kittipat25er@gmail.com>
2025-12-02 05:33:23 +0700

    Add Next.js and Minecraft docs, update main index
    Introduces new documentation sections for Next.js and Minecraft, including metadata and starter pages. Updates the main docs index with a new overview and project references. Adjusts browser and server source files to register new docs. Updates global CSS to import additional Fumadocs UI styles and comments out custom color variables.

--- .source/server.ts
+++ .source/server.ts
@@ -1,4 +1,9 @@
 // @ts-nocheck
+import { default as __fd_glob_5 } from "../content/docs/nextjs/meta.json?collection=docs"
+import { default as __fd_glob_4 } from "../content/docs/minecraft/meta.json?collection=docs"
+import * as __fd_glob_3 from "../content/docs/nextjs/quick-started-nextjs.mdx?collection=docs"
+import * as __fd_glob_2 from "../content/docs/nextjs/deploy.mdx?collection=docs"
+import * as __fd_glob_1 from "../content/docs/minecraft/index.mdx?collection=docs"
 import * as __fd_glob_0 from "../content/docs/index.mdx?collection=docs"
 import { server } from 'fumadocs-mdx/runtime/server';
 import type * as Config from '../src/lib/source.config';
@@ -8,4 +13,4 @@ const create = server<typeof Config, import("fumadocs-mdx/runtime/types").Intern
   }
 }>({"doc":{"passthroughs":["extractedReferences"]}});
 
-export const docs = await create.docs("docs", "content/docs", {}, {"index.mdx": __fd_glob_0, });
+export const docs = await create.docs("docs", "content/docs", {"minecraft/meta.json": __fd_glob_4, "nextjs/meta.json": __fd_glob_5, }, {"index.mdx": __fd_glob_0, "minecraft/index.mdx": __fd_glob_1, "nextjs/deploy.mdx": __fd_glob_2, "nextjs/quick-started-nextjs.mdx": __fd_glob_3, });
