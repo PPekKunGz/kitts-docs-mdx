87473d91af40f7395fff82709852916c858b75c3
PPekKunGzDev <kittipat25er@gmail.com>
2025-12-11 00:41:57 +0700

    Add TrueMoney voucher API documentation
    Introduced new documentation for the TrueMoney Gift API under docs/topup, including meta.json and truemoney-voucher.mdx. Updated browser and server source files to register the new documentation section.

--- .source/server.ts
+++ .source/server.ts
@@ -1,11 +1,13 @@
 // @ts-nocheck
-import * as __fd_glob_7 from "../content/docs/minecraft/mods/waypoint-warp.mdx?collection=docs"
-import * as __fd_glob_6 from "../content/docs/minecraft/index.mdx?collection=docs"
-import * as __fd_glob_5 from "../content/docs/nextjs/quick-started-nextjs.mdx?collection=docs"
-import * as __fd_glob_4 from "../content/docs/nextjs/deploy.mdx?collection=docs"
-import * as __fd_glob_3 from "../content/docs/index.mdx?collection=docs"
-import { default as __fd_glob_2 } from "../content/docs/minecraft/mods/meta.json?collection=docs"
-import { default as __fd_glob_1 } from "../content/docs/nextjs/meta.json?collection=docs"
+import * as __fd_glob_9 from "../content/docs/minecraft/mods/waypoint-warp.mdx?collection=docs"
+import * as __fd_glob_8 from "../content/docs/topup/truemoney-voucher.mdx?collection=docs"
+import * as __fd_glob_7 from "../content/docs/nextjs/quick-started-nextjs.mdx?collection=docs"
+import * as __fd_glob_6 from "../content/docs/nextjs/deploy.mdx?collection=docs"
+import * as __fd_glob_5 from "../content/docs/minecraft/index.mdx?collection=docs"
+import * as __fd_glob_4 from "../content/docs/index.mdx?collection=docs"
+import { default as __fd_glob_3 } from "../content/docs/minecraft/mods/meta.json?collection=docs"
+import { default as __fd_glob_2 } from "../content/docs/nextjs/meta.json?collection=docs"
+import { default as __fd_glob_1 } from "../content/docs/topup/meta.json?collection=docs"
 import { default as __fd_glob_0 } from "../content/docs/minecraft/meta.json?collection=docs"
 import { server } from 'fumadocs-mdx/runtime/server';
 import type * as Config from '../src/lib/source.config';
@@ -15,4 +17,4 @@ const create = server<typeof Config, import("fumadocs-mdx/runtime/types").Intern
   }
 }>({"doc":{"passthroughs":["extractedReferences"]}});
 
-export const docs = await create.docs("docs", "content/docs", {"minecraft/meta.json": __fd_glob_0, "nextjs/meta.json": __fd_glob_1, "minecraft/mods/meta.json": __fd_glob_2, }, {"index.mdx": __fd_glob_3, "nextjs/deploy.mdx": __fd_glob_4, "nextjs/quick-started-nextjs.mdx": __fd_glob_5, "minecraft/index.mdx": __fd_glob_6, "minecraft/mods/waypoint-warp.mdx": __fd_glob_7, });
+export const docs = await create.docs("docs", "content/docs", {"minecraft/meta.json": __fd_glob_0, "topup/meta.json": __fd_glob_1, "nextjs/meta.json": __fd_glob_2, "minecraft/mods/meta.json": __fd_glob_3, }, {"index.mdx": __fd_glob_4, "minecraft/index.mdx": __fd_glob_5, "nextjs/deploy.mdx": __fd_glob_6, "nextjs/quick-started-nextjs.mdx": __fd_glob_7, "topup/truemoney-voucher.mdx": __fd_glob_8, "minecraft/mods/waypoint-warp.mdx": __fd_glob_9, });
